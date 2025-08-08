e1dda55953eaefc618f4ed965350a830f4f97ee3
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-08-09 03:30:03 +0530

    initial commit [ Header ✅ ]

---
+++ script.js
@@ -0,0 +1,24 @@
+function intialize() {
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // Create link for Poppins font
+        document.head.appendChild(
+            Object.assign(document.createElement('link'), {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Poppins:100,regular'
+            })
+        );
+
+        document.head.appendChild(
+            Object.assign(document.createElement('script'), {
+                crossorigin: 'anonymous',
+                src: 'https://kit.fontawesome.com/0f0486bb75.js'
+            })
+        );
+
+
+    });
+
+}
+intialize();
